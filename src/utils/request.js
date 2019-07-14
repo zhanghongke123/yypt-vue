a0807343c72ae5a020fce816fbbab51503cd4632
zhanghongke123 <18237716257@163.com>
2019-07-15 06:44:48 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: "http://192.168.31.116:8099", // url = base url + request url
+  baseURL: "http://127.0.0.1:8099", // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 50000 // request timeout
 })
