313ea80c789ee96371ba4eb0e3717c5c42ac916a
zhanghongke123 <18237716257@163.com>
2019-08-07 06:58:28 +0800

    表格组件

---
+++ src/components/YyptTable/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+   <el-table ref="table" v-loading="listLoading" :data="tabledata" height="50vh" @cell-dblclick="dblclick" @selection-change="handleSelectionChange"
+         @current-change="rowchange" border highlight-current-row >
+          <el-table-column v-if="multiselect" type="selection" width="55"></el-table-column>
+          <el-table-column v-for="col in datacolumn" :key="col.prop"  :width="col.width" align="center" :label="col.label">
+              <template slot-scope="scope">
+                <span v-if="col.iscombox">{{ getcomboxvalue(col.comboxvalue, scope.row[col.prop]) }}</span>
+                <span v-else> {{  scope.row[col.prop] }}</span>
+              </template>
+          </el-table-column>
+    </el-table>
+        
+    </div>   
+</template>
+<script>
+export default {
+    name:"YyptTable",
+    props:['datacolumn','tabledata'],
+    data(){
+        return {
+            listLoading:false
+        }
+    }
+}
+</script>
+
